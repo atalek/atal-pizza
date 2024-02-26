2e059351ecb2c63ec30f67e717cb086ad81f1b04
Amphei <aleksandaratanasovski1994@gmail.com>
2024-02-26 18:05:07 +0100

    added target blank to a link

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,7 @@ const currentYear = new Date().getFullYear()
           class="fixed bg-gradient-to-tr from-white to-emerald-50 z-[-1] inset-0 opacity-50" />
       </div>
       <footer class="border-t p-4 text-center text-slate-600 mt-16">
-        <NuxtLink to="https://github.com/Amphei/atal-pizza"
+        <NuxtLink to="https://github.com/Amphei/atal-pizza" target="_blank"
           >&copy; Atal Pizza - {{ currentYear }} made by Amphei
           <Icon name="fa6-brands:github" class="w-6 h-6"
         /></NuxtLink>
